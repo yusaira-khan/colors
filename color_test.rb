35787b053375beb0c56f64b1bad9f5f6f994dd9a
Yusaira Khan <yusairak@nulogy.com>
2021-08-03 19:23:22 -0400

    add red test

--- color_test.rb
+++ color_test.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
-def hsb
-
+def rgb(red:)
+  return "#ff0000"
 end
 class ColorTest < Minitest::Test
 
-  def test_hsb
-    hsb
+  def test_red
+    assert_equal(rgb(red:100),"#ff0000")
   end
 end
