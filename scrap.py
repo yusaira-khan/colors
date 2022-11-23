a3251449795449202b13d6ec92bb374a1c5d3d15
Yusaira Khan <yusairak@nulogy.com>
2022-11-23 13:08:26 -0500

    try illuminants

--- scrap.py
+++ scrap.py
@@ -1,6 +1,10 @@
-import numpy
+import numpy as np
 import skimage
 
-rgb = numpy.array([[0, 0, 255]])
-lab = skimage.color.rgb2lab(rgb, illuminant='D50')
-print(lab)
+rgb = skimage.io.imread("png/red.png")[0][0]
+print(rgb)
+rgbarr = np.array(rgb)
+d50r = skimage.color.rgb2lab(rgb, illuminant='D50')
+d65r = skimage.color.rgb2lab(rgb, illuminant='D65')
+d75r = skimage.color.rgb2lab(rgb, illuminant='D75')
+print(d50r, d65r, d75r)
